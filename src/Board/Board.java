cd7570bbc73ca06b622821c1a347119b9c2e8516
lostkid456 <43279634+lostkid456@users.noreply.github.com>
2018-12-05 15:04:19 -0500

    Update Board.java

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -10,13 +10,13 @@ public class Board {
     }
 
     public void print() {
-        String row = "";
+        String row ="";
         for (int i = 0; i < board.length; i++) {
             row = "";
             for (int j = 0; j < board[i].length; j++) {
-                row+=board[i][j].toString();
+                row += board[i][j].toString();
             }
+            System.out.println(row);
         }
-        System.out.println(row);
     }
 }
